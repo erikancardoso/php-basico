4110013347c34b2810f3b0be58d637151ed78917
erikancardoso <erikancardoso@outlook.com>
2023-02-22 02:15:09 -0300

    Repeticao

---
+++ repeticao.php
@@ -0,0 +1,15 @@
+<?php
+
+# while contagens com detalhes
+$i = 0;
+while ($i < 10) {
+    echo "executou $i <br> ";
+    $i = $i +1;
+}
+
+# for contagens simples
+for ($j = 1; $j <= 10; $j++){
+      echo "contagem $j <br>";
+}
+
+# foreach para percorrer arrays
